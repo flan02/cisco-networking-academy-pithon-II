29818c9a136ab566f1437a8662be06decbc8f2d1
flan02 <chanivetdan@hotmail.com>
2024-10-21 21:31:16 -0300

    module 3 - section 1 completed.

---
+++ module-3/section-1-foundations_OPP.py
@@ -0,0 +1,26 @@
+
+# $ The basic concepts of the object-oriented approach
+
+# - A pink Cadillac went quickly.
+# ? Object name = Cadillac
+# ? Home class = Wheeled vehicles
+# ? Property = Color (pink)
+# ? Activity = Go (quickly)
+
+# - Rudolph is a large cat who sleeps all day.
+#? Object name = Rudolph
+#? Home class = Cat
+#? Property = Size (large)
+#? Activity = Sleep (all day)
+
+""" 
+Object programming is the art of defining and expanding classes. A class is a model of a very specific part of reality, reflecting properties and activities found in the real world.
+"""
+
+# TODO: Create a CLASS
+class TheSimplestClass:
+    pass # The pass statement is used as a placeholder for future code.
+
+# TODO: Create an OBJECT (instantiation)
+my_first_object = TheSimplestClass() # The object is created by calling the class as if it were a function.
+
